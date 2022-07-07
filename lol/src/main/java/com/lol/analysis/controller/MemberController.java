f62838fbd34b0d00e4c3749cf58d07a1b0f34490
smokeperfume <wjdgnstls97@naver.com>
2022-07-07 16:30:11 +0900

    로그인 기능 구현, 헤더 로그아웃 구현, 컨트롤러 수정 *(session에 접속자 표시 필요, 접속자에 따른 메뉴 변경 필요)

--- lol/src/main/java/com/lol/analysis/controller/MemberController.java
+++ lol/src/main/java/com/lol/analysis/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.lol.analysis.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,6 +31,23 @@ public class MemberController {
 	public ModelAndView login(ModelAndView model) {
 		model.setViewName("/member/login");
 		return model;
+	}
+	
+	@PostMapping("/login.do")
+	public String login(String id, String pw, HttpSession session) {
+		MemberVo memberVo=mr.findByIdAndPw(id, pw);
+		if(memberVo!=null) {
+			session.setAttribute("memberVo", memberVo);
+			return "redirect:/";
+		}else {
+			return "redirect:/member/login";
+		}
+	}
+	
+	@GetMapping("/logout.do")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
 	}
 	
 	@GetMapping("/signup.do")
--- lol/src/main/java/com/lol/analysis/repository/MemberRepository.java
+++ lol/src/main/java/com/lol/analysis/repository/MemberRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.repository.CrudRepository;
 import com.lol.analysis.vo.MemberVo;
 
 public interface MemberRepository extends CrudRepository<MemberVo, String>{
+	//멤버리스트 출력	
 	public Page<MemberVo> findAll(Pageable pageable);
 	
+	//로그인 id, pw 확인
+	public MemberVo findByIdAndPw(String id, String pw);
 
 }
