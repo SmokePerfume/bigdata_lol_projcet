0cb8d8513e41d3d111ac49948075e427572dc483
smokeperfume <wjdgnstls97@naver.com>
2022-07-14 17:44:33 +0900

    python 명령어 사용시 오류 python3으로 변경 서버내부에 있는 파이썬이용

--- lol/src/main/java/com/lol/analysis/controller/TestController.java
+++ lol/src/main/java/com/lol/analysis/controller/TestController.java
@@ -26,11 +26,11 @@ public class TestController {
 	public ModelAndView login(ModelAndView model) throws Exception{
 		model.setViewName("test/case1");
 		////////////////
-		System.out.println("Python Call");
+		System.out.println("Python3 Call");
         String[] command = new String[4];
-        command[0] = "python";
+        command[0] = "python3";
         //command[1] = "\\workspace\\java-call-python\\src\\main\\resources\\test.py";
-        command[1] = System.getProperty("user.dir")+"\\src\\PyStorage\\hello.py";
+        command[1] = resolvePythonScriptPath("hello.py");
         System.out.println("현재경로 : "+ command[1]);
         command[2] = "10";
         command[3] = "20";
@@ -63,7 +63,7 @@ public class TestController {
 	
 	
     private String resolvePythonScriptPath(String filename) {
-        File file = new File("src/test/resources/" + filename);
+        File file = new File("src/PyStorage/" + filename);
         return file.getAbsolutePath();
     }
 }
