be9168a2b0b708289e0fdb8623c4beb6fa008b94
smokeperfume <wjdgnstls97@naver.com>
2022-07-14 12:12:42 +0900

    개인 컴퓨터의 파이썬 이용, test추가 pom.xml 외부 프로그램 사용 의존성 추가, 테스트용 py 추가, 간단히 출력할 화면의 경로 추가 (츄가적인 입력폼, 대화형 출력화면 대책 강구)

---
+++ lol/src/main/java/com/lol/analysis/controller/TestController.java
@@ -0,0 +1,69 @@
+package com.lol.analysis.controller;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("/case1")
+	public ModelAndView login(ModelAndView model) throws Exception{
+		model.setViewName("test/case1");
+		////////////////
+		System.out.println("Python Call");
+        String[] command = new String[4];
+        command[0] = "python";
+        //command[1] = "\\workspace\\java-call-python\\src\\main\\resources\\test.py";
+        command[1] = System.getProperty("user.dir")+"\\src\\PyStorage\\hello.py";
+        System.out.println("현재경로 : "+ command[1]);
+        command[2] = "10";
+        command[3] = "20";
+        try {
+            execPython(command);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+		////////////
+		return model;
+	}
+
+	
+	public static void execPython(String[] command) throws IOException, InterruptedException {
+        CommandLine commandLine = CommandLine.parse(command[0]);
+        for (int i = 1, n = command.length; i < n; i++) {
+            commandLine.addArgument(command[i]);
+        }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(outputStream);
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setStreamHandler(pumpStreamHandler);
+        int result = executor.execute(commandLine);
+        System.out.println("result: " + result);
+        System.out.println("output: " + outputStream.toString());
+
+    }  
+	
+	
+    private String resolvePythonScriptPath(String filename) {
+        File file = new File("src/test/resources/" + filename);
+        return file.getAbsolutePath();
+    }
+}
