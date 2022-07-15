7659d353fd159cd325346869028be88cee9c0b7b
smokeperfume <wjdgnstls97@naver.com>
2022-07-15 15:39:49 +0900

    사용자가 데이터 입력후 예측하기 누를시 파이썬과 연동 확인 controller이 알맞은 데이터를 받도록 html의 input name 설정

--- lol/src/main/java/com/lol/analysis/controller/TestController.java
+++ lol/src/main/java/com/lol/analysis/controller/TestController.java
@@ -3,12 +3,12 @@ package com.lol.analysis.controller;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-
-
+import org.springframework.ui.Model;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
@@ -19,7 +19,10 @@ import org.apache.commons.exec.PumpStreamHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.lol.analysis.vo.MemberVo;
 
 @Controller
 @RequestMapping("/test")
@@ -33,7 +36,7 @@ public class TestController {
 	}
 	
 	@PostMapping("/case1.do")
-	public String case1(String data1, String data2) throws Exception{
+	public String case1(String inputdata1, String inputdata2) throws Exception{
 		////////////////
 		System.out.println("Python3 Call");
         String[] command = new String[4];
@@ -41,21 +44,30 @@ public class TestController {
         //command[1] = "\\workspace\\java-call-python\\src\\main\\resources\\test.py";
         command[1] = resolvePythonScriptPath("hello.py");
         System.out.println("현재경로 : "+ command[1]);
-        command[2] = "10";
-        command[3] = "20";
+        command[2] = inputdata1;
+        command[3] = inputdata2;
         try {
             execPython(command);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "redirect:/";
+        return "redirect:/test/case1.do";
 	}
 
-	
+//    @GetMapping("/update.do")
+//	public String update(Model model, @RequestParam(name="id") String id) {
+//        Optional<MemberVo> memOption = mr.findById(id);
+//        MemberVo memberVo=memOption.get();
+//        model.addAttribute("memberVo",memberVo);
+//		return "member/update";
+//	}
+    
+    
 	public static void execPython(String[] command) throws IOException, InterruptedException {
         CommandLine commandLine = CommandLine.parse(command[0]);
         for (int i = 1, n = command.length; i < n; i++) {
             commandLine.addArgument(command[i]);
+            System.out.print(command[i]+" ");
         }
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
