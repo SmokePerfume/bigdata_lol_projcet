a61724ae724caf2281fd0d17ac4592c7d876d152
smokeperfume <wjdgnstls97@naver.com>
2022-07-21 17:43:31 +0900

    필요없는 주석 제거, 컨트롤러명 변경, 값을 주고받기 추가 model을 활용하여 값을 넘기도록 변경

--- lol/src/main/java/com/lol/analysis/controller/ServiceController.java
+++ lol/src/main/java/com/lol/analysis/controller/ServiceController.java
@@ -34,7 +34,7 @@ import com.lol.analysis.vo.MemberVo;
 
 @Controller
 @RequestMapping("/service")
-public class TestController {
+public class ServiceController {
 	@Autowired
 	MemberRepository mr;
 	
@@ -44,59 +44,13 @@ public class TestController {
 	
 	@GetMapping("/test1.do")
 	public String case1(Model model){
-			//정상 작동 확인 나중에 목록 넘길시 사용
-		//System.out.println(cr.findAll());
-		//model.addAttribute("chList", cr.findAll());
-		
-		
-
-//		Gson gson = new Gson();
-//		JsonArray jArray = new JsonArray();
-//		System.out.println(mr.findGenderCountWithMember());
-		//Iterator<MemGenderCntVo> it = mr.findGenderCountWithMember().iterator();
-		//System.out.println(it);
-		
-		//while(it.hasNext()) {
-		//	System.out.println(it.next().toString());
-			//LogVO curVO = it.next();
-//			JsonObject object = new JsonObject();
-//			String userid = curVO.getLog_userid();
-//			int cnt = curVO.getCnt();
-//			
-//		    object.addProperty("ID", userid);
-//			object.addProperty("Count", cnt);
-//			jArray.add(object);
-	//	}
-//				
-//		String json = gson.toJson(jArray);
-//		model.addAttribute("json", json);
-		
+		model.addAttribute("chList", cr.findAll());
 		return "service/test1";
 	}
 	
-//	@PostMapping("/case1.do")
-//	public String case1(String reddata1, String reddata2) throws Exception{
-//		System.out.println("Python3 Call");
-//        String[] command = new String[4];
-//        command[0] = "python";
-//        command[1] = "\\workspace\\java-call-python\\src\\main\\resources\\test.py";
-//        command[1] = resolvePythonScriptPath("hello.py");
-//        System.out.println("현재경로 : "+ command[1]);
-//        command[2] = reddata1;
-//        command[3] = reddata2;
-//        try {
-//            execPython(command);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return "redirect:/test/case1.do";
-//	}
-	
 	@PostMapping("/test1.do")
 	public String case1(String reddata1, String reddata2,String reddata3,String reddata4,String reddata5,
-			String bluedata1,String bluedata2,String bluedata3,String bluedata4,String bluedata5) throws Exception{
-		
-		//pip3 install -U scikit-learn
+			String bluedata1,String bluedata2,String bluedata3,String bluedata4,String bluedata5, Model model) throws Exception{
 		
 		System.out.println("Python3 Call test1");
 		String[] command = new String[12];
@@ -116,24 +70,16 @@ public class TestController {
 	    command[11] = bluedata5;
 		
 		try {
-			execPython(command);
+			model.addAttribute("predresult", execPython(command));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
         return "redirect:/service/test1.do";
 	}
-
-//    @GetMapping("/update.do")
-//	public String update(Model model, @RequestParam(name="id") String id) {
-//        Optional<MemberVo> memOption = mr.findById(id);
-//        MemberVo memberVo=memOption.get();
-//        model.addAttribute("memberVo",memberVo);
-//		return "member/update";
-//	}
     
     
-	public static void execPython(String[] command) throws IOException, InterruptedException {
+	public static String execPython(String[] command) throws IOException, InterruptedException {
         CommandLine commandLine = CommandLine.parse(command[0]);
         for (int i = 1, n = command.length; i < n; i++) {
             commandLine.addArgument(command[i]);
@@ -147,6 +93,7 @@ public class TestController {
         int result = executor.execute(commandLine);
         System.out.println("result: " + result);
         System.out.println("output: " + outputStream.toString());
+        return outputStream.toString();
 
     }  
 	
