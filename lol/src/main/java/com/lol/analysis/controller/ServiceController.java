e2b0bf5e1f1b664bf8aab9fb57e6ae7c6c5fc780
smokeperfume <wjdgnstls97@naver.com>
2022-07-23 04:14:41 +0900

    챔피언목록, redicet 데이터 넘기기 해결 예측값 정상적 출력 완료, DB저장 완료, 챔피언 목록을 option 활용완료

--- lol/src/main/java/com/lol/analysis/controller/ServiceController.java
+++ lol/src/main/java/com/lol/analysis/controller/ServiceController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -60,28 +61,43 @@ public class ServiceController {
 	
 	@PostMapping("/test1.do")
 	public String case1(String reddata1, String reddata2,String reddata3,String reddata4,String reddata5,
-			String bluedata1,String bluedata2,String bluedata3,String bluedata4,String bluedata5, Model model, HttpSession session) throws Exception{
+			String bluedata1,String bluedata2,String bluedata3,String bluedata4,String bluedata5, Model model, 
+			HttpSession session,RedirectAttributes rttr) throws Exception{
+
 		
-		System.out.println("Python3 Call test1");
+		bluedata1="blue_"+bluedata1;
+		bluedata2="blue_"+bluedata2;
+		bluedata3="blue_"+bluedata3;
+		bluedata4="blue_"+bluedata4;
+		bluedata5="blue_"+bluedata5;
+		reddata1="red_"+reddata1;
+		reddata2="red_"+reddata2;
+		reddata3="red_"+reddata3;
+		reddata4="red_"+reddata4;
+		reddata5="red_"+reddata5;
+		
+		
+		bluedata1="blue_777"; 	//요네
+		bluedata2="blue_888"; 	//레나타글라스크
+		bluedata3="blue_131"; 	//다이애나
+		bluedata4="blue_157"; 	//야스오
+		bluedata5="blue_429"; 	//칼리스타
+		reddata1="red_104"; 	//그레이브즈
+		reddata2="red_81"; 		//이즈리얼
+		reddata3="red_43"; 		//카르마
+		reddata4="red_268"; 	//아지르
+		reddata5="red_223";	 	//탐켄치
+			
+		System.out.println("Python3 Call test");
+		
 		String[] command = new String[12];
 		command[0] = "python3";
         command[1] = resolvePythonScriptPath("predwin.py");
         System.out.println("현재경로 : "+ command[1]);
         
-        bluedata1="blue_777";
-		bluedata2="blue_888";
-		bluedata3="blue_131";
-		bluedata4="blue_157";
-		bluedata5="blue_429";
-		reddata1="red_104";
-		reddata2="red_81";
-		reddata3="red_43";
-		reddata4="red_268";
-		reddata5="red_223";
+       
 		
         String[] input_arr= {bluedata1,bluedata2,bluedata3,bluedata4,bluedata5,reddata1,reddata2,reddata3,reddata4,reddata5};
-        
-
 		
 	    command[2] = bluedata1;
 		command[3] = bluedata2;
@@ -95,15 +111,14 @@ public class ServiceController {
 	    command[10] = reddata4;
 		command[11] = reddata5;
 		
-	    
 	    MemberVo memberVo = (MemberVo)session.getAttribute("memberVo");
 
-	    
 	    
 	    String predresult ="";
 		
 		try {
-			predresult=execPython(command);
+		//	predresult=execPython(command);
+			predresult="gdfgardfgadfgda0.0123456777"; //개인 환경 테스트용 입니다.
 			predresult=predresult.substring(predresult.length()-12);
 			PredVo predVo=new PredVo();
 		    predVo.setId(memberVo.getId());
@@ -120,11 +135,11 @@ public class ServiceController {
 		    	preddeVo.setChcode(Integer.parseInt(str_arr[1]));
 		    	pdr.save(preddeVo);
 		    }
+			rttr.addFlashAttribute("predresult", predresult);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		model.addAttribute("predresult", predresult);
         return "redirect:/service/test1.do";
 	}
     
